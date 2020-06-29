e4e1240bfbfea641e5df1a6bb09904342a7c7fe2
Szabolcs Ferenczi <szabolcs.ferenczi@aerlingus.com>
2020-06-29 18:35:29 +0100

    Updated server settings.

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const serveStatic = require("serve-static");
 const path = require("path");
-let app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
+const dotenv = require("dotenv");
+dotenv.config();
+const app = express();
+// All urls goto to index.html in /dist folder [build folder]
+app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.get(/.*/, function(req, res) {
+  res.sendFile(path.join(__dirname, "/dist/index.html"));
+});
 const port = process.env.PORT || 8081;
 app.listen(port);
-console.log("server started " + port);
+console.log(`App is listening on port: ${port}`);
