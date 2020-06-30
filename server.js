4d625621099fefed7446beb587a7fdd1db23a89d
valentino22 <szabi22@gmail.com>
2020-06-30 15:20:58 +0100

    Adding express to dependencies.

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-// All urls goto to index.html in /dist folder [build folder]
 app.use("/", serveStatic(path.join(__dirname, "/dist")));
+
+// All urls goto to index.html
 app.get(/.*/, function(req, res) {
   res.sendFile(path.join(__dirname, "/dist/index.html"));
 });
+
 const port = process.env.PORT || 8081;
 app.listen(port);
 console.log(`App is listening on port: ${port}`);
